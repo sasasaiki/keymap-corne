2370d1961c52fe054c36cf4b984906e85b9182cd
saiki <otoiawasehakotiradayo@gmail.com>
2019-07-12 11:04:49 +0900

    エンターとかと切り替え逆にしてみた

--- keyboards/crkbd/keymaps/saikis_thefrey/keymap.c
+++ keyboards/crkbd/keymaps/saikis_thefrey/keymap.c
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|------+------+------+------+------+------|                |------+------+------+------+------+------|
        LSFT,     Z,     X,     C,     V,     B,                      N,     M,  COMM,   DOT,  SLSH,  RSFT,\
   //|------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
-                                  GUIEI, LOWER,   SPC,      ENT, RAISE, ALTKN \
+                                  GUIEI,   SPC, LOWER,    RAISE,   ENT, ALTKN \
                               //`--------------------'  `--------------------'
   ),
  
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|------+------+------+------+------+------|                |------+------+------+------+------+------|
        LSFT,    F1,    F2,    F3,    F4,    F5,                     F6,    F7,    F8,    F9,   F10,  RSFT,\
   //|------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
-                                  GUIEI, LOWER, ALTKN,    GUIEI, RAISE, ALTKN \
+                                  GUIEI, ALTKN, LOWER,    RAISE, GUIEI, ALTKN \
                               //`--------------------'  `--------------------'
   ),
 
@@ -90,7 +90,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|------+------+------+------+------+------|                |------+------+------+------+------+------|
        LSFT, XXXXX, XXXXX, XXXXX, XXXXX, XXXXX,                   UNDS,  PLUS,  LBRC,  RBRC,  BSLS,  TILD,\
   //|------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
-                                  GUIEI, LOWER, ALTKN,    GUIEI, RAISE, ALTKN \
+                                  GUIEI, ALTKN, LOWER,    RAISE, GUIEI, ALTKN \
                               //`--------------------'  `--------------------'
   ),
 
@@ -102,7 +102,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|------+------+------+------+------+------|                |------+------+------+------+------+------|
        LMOD,  LHUD,  LSAD,  LVAD, XXXXX, XXXXX,                  XXXXX, XXXXX, XXXXX, XXXXX, XXXXX, XXXXX,\
   //|------+------+------+------+------+------+------|  |------+------+------+------+------+------+------|
-                                  GUIEI, LOWER, ALTKN,    GUIEI, RAISE, ALTKN \
+                                  GUIEI, ALTKN, LOWER,    RAISE, GUIEI, ALTKN \
                               //`--------------------'  `--------------------'
   )
 };
